ab5e1c2b46c3b9cb19fd96d8e760f57d2660da9b
Pedro <pedro.pinhal@gmail.com>
2015-10-20 23:19:41 +0100

    using ninject

--- BusDemo/Class1.cs
+++ BusDemo/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Timers;
+using Ninject;
 using Rebus.Activation;
 using Rebus.Config;
 using Rebus.Handlers;
@@ -13,27 +14,51 @@ namespace BusDemo
     {
         public static void Main()
         {
+            /*
             const string inputQueueName = "my-app.input";
             using (var activator = new BuiltinHandlerActivator())
             using (var timer = new Timer())
             {
                 activator.Register(() => new PrintDateTime());
-
+               
                 var bus = Configure.With(activator)
                     .Transport(t => t.UseMsmq(inputQueueName))
                     .Routing(r => r.TypeBased().Map<DateTime>(inputQueueName))
                     .Start();
 
-                timer.Elapsed += delegate { bus.Send(DateTime.Now).Wait(); };
+                timer.Elapsed += delegate
+                {
+                    bus.Send("hello").Wait();
+                    bus.Send(DateTime.Now).Wait(); 
+                    
+                };
                 timer.Interval = 1000;
                 timer.Start();
 
                 Console.WriteLine("Press enter to quit");
                 Console.ReadLine();
-            }
+                */
+            IKernel kernel = new StandardKernel();
+            
+            kernel.Bind<IContainerAdapter>().To<Rebus.Ninject.NinjectContainerAdapter>().
+                WithConstructorArgument("kernel", kernel);
+            kernel.Bind<IHandleMessages<TestEvent>>().To<TestHandler>();
 
+            var _adapter = kernel.Get<IContainerAdapter>();
+
+            var bus = Configure.With(_adapter)
+                .Transport(t => t.UseMsmq("inputQueueName"))
+                .Routing(r => r.TypeBased().Map<TestEvent>("inputQueueName"))
+                .Start();
+
+            bus.Send(new TestEvent()).Wait();
+            Console.ReadLine();
         }
+
     }
+
+        
+    
 
     public class PrintDateTime : IHandleMessages<DateTime>
     {
@@ -42,5 +67,19 @@ namespace BusDemo
             Console.WriteLine("The time is {0}", message);
         }
     
+    }
+
+    public class TestHandler : IHandleMessages<TestEvent>
+    {
+      
+
+        public async Task Handle(TestEvent message)
+        {
+            Console.WriteLine("The message is {0}", message);
+        }
+    }
+
+    public class TestEvent
+    {
     }
 }
